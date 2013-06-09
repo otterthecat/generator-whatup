421d9d5d98ee9b388296cdd3291dbcbd26a93300
Otter <otc@otterthecat.com>
2013-06-09 18:02:21 -0500

    Updated package.json template Added option to include GitHub screen name

--- app/index.js
+++ app/index.js
@@ -39,6 +39,10 @@ WhatupGenerator.prototype.askFor = function askFor() {
     message: 'Whatup - what you wanna call your project?',
     default: 'myProject'
   }, {
+    name: 'ghAcct',
+    message: 'Whatup - enter your GitHub screen name',
+    default: "brocksampson"
+  },{
     name: 'useLess',
     message: 'Whatup - you wanna use LESS?',
     default: 'Y/n',
@@ -56,6 +60,8 @@ WhatupGenerator.prototype.askFor = function askFor() {
 
     this.projectName = props.projectName;
     this.useLess = (/y/i).test(props.useLess);
+    this.ghAcct = props.ghAcct;
+    this.testFramework = props.testFramework.toLowerCase();
 
     cb();
   }.bind(this));
@@ -65,12 +71,32 @@ WhatupGenerator.prototype.app = function app() {
   this.mkdir('app');
   this.mkdir('app/templates');
 
+  // LESS support
   if(this.useLess){
     this.mkdir('less');
   }
 
-  this.copy('_package.json', 'package.json');
+  // test frameworks
+  this.testFw = {
+    name: "grunt-contrib-jasmine",
+    version: "~0.4.2"
+  }
+
+  if(this.testFramework === "qunit"){
+
+    this.testFw.name = "grunt-contrib-qunit",
+    this.testFw.version = "~0.1.0"
+  } else if(this.testFramework !== "jasmine"){
+
+    console.log("I don't know what " + this.testFramework + " is, so I'm going to default to Jasmine");
+  }
+
+  //this.copy('_package.json', 'package.json');
   this.copy('_bower.json', 'bower.json');
+};
+
+WhatupGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
 };
 
 WhatupGenerator.prototype.projectfiles = function projectfiles() {
